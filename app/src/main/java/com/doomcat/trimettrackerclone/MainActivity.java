2a06313b0b7a394f4b3bdfcb8345d1b8a35fcf29
znergy <ryanjonesirl@gmail.com>
2017-05-23 16:54:31 -0700

    add menu items

--- app/src/main/java/com/doomcat/trimettrackerclone/MainActivity.java
+++ app/src/main/java/com/doomcat/trimettrackerclone/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -13,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     @Override
